3716dd4f0226ec3bcecf97e597d3ab5e68131a7f
Michael Reel <michael.reel@civica.co.uk>
2016-05-20 15:28:48 +0100

    Runs in about half the time now.

--- BrokenKeyboard/run_longest_words.py
+++ BrokenKeyboard/run_longest_words.py
@@ -1,4 +1,4 @@
-import time
+from time import time
 
 input_01 = ("3\nabcd\nqwer\nhjklo")
 input_02 = ("4\nedcf\nbnik\npoil\nvybu")
@@ -6,23 +6,29 @@ input_02 = ("4\nedcf\nbnik\npoil\nvybu")
 dictFile = 'enable1.txt'
 
 def getLongestWords(inputStr, dictFile):
+    startTime = time()
+        
     inputArr = inputStr.split("\n");
     outputArr = ["" for x in range(int(inputArr.pop(0)))]
     
-    with open(dictFile) as df:
-        for word in df:
-            word = word.strip()
-            for k in range(len(inputArr)):
-                if set(word).issubset(inputArr[k]) and len(outputArr[k]) < len(word):
-                    outputArr[k] = word
+    wordDict = {}
+    
+    with open(dictFile) as words:
+        for w in words:
+            w = w.strip()
+            key = frozenset(w)
+            if not key in wordDict or len(wordDict[key]) < len(w):
+                wordDict[key] = w
+                # Only update words if dictionary updated
+                for k in range(len(inputArr)):
+                    if key.issubset(inputArr[k]) and len(outputArr[k]) < len(w):
+                        outputArr[k] = w
     
     for k in range(len(inputArr)):
         print "{} = {}".format(inputArr[k], outputArr[k])
+        
+    print "Time taken: {}".format(time() - startTime)
     
-startTime = time.time()
 getLongestWords(input_01, dictFile)
-print "Time taken: {}".format(time.time() - startTime)
 
-startTime = time.time()
 getLongestWords(input_02, dictFile)
-print "Time taken: {}".format(time.time() - startTime)
