1e668785e5e9f7a0d79564866bbb4344bb3b2c1e
Michael Reel <michael.reel@civica.co.uk>
2016-05-23 17:03:07 +0100

    A few more optimisations.

--- BrokenKeyboard/run_longest_words.py
+++ BrokenKeyboard/run_longest_words.py
@@ -1,9 +1,17 @@
 from time import time
+from itertools import combinations
 
 input_01 = ("3\nabcd\nqwer\nhjklo")
 input_02 = ("4\nedcf\nbnik\npoil\nvybu")
 
 dictFile = 'enable1.txt'
+
+def getSubsets(s):
+    res = []
+    for i in range(1, len(s) + 1):
+        for combo in combinations(s, i):
+            res.append(frozenset(combo))
+    return res
 
 def getLongestWords(inputStr, dictFile):
     startTime = time()
@@ -19,16 +27,17 @@ def getLongestWords(inputStr, dictFile):
             key = frozenset(w)
             if not key in wordDict or len(wordDict[key]) < len(w):
                 wordDict[key] = w
-                # Only update words if dictionary updated
-                for k in range(len(inputArr)):
-                    if key.issubset(inputArr[k]) and len(outputArr[k]) < len(w):
-                        outputArr[k] = w
+
+    for k in range(len(inputArr)):
+        for key in getSubsets(frozenset(inputArr[k])):
+            if key in wordDict and len(wordDict[key]) > len(outputArr[k]):
+                outputArr[k] = wordDict[key]
     
     for k in range(len(inputArr)):
         print "{} = {}".format(inputArr[k], outputArr[k])
         
     print "Time taken: {}".format(time() - startTime)
-    
+
 getLongestWords(input_01, dictFile)
 
 getLongestWords(input_02, dictFile)
