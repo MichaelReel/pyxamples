3fb8f50d9c43a629bacca7ff5f48063050b7e0c5
Michael Reel <michael.reel@civica.co.uk>
2016-06-01 16:56:05 +0100

    Bit test now runs all the primary value generators.

--- Drawing/run_bit_test.py
+++ Drawing/run_bit_test.py
@@ -1,32 +1,41 @@
 from procedural import rand, gradient, knuth, jenkins, wang, builtin, perlin, perlin_ref
+import sys
+from time import time
 
 width = 200
 height = 200
 
 gridSize = (width, height)
 
-# generator = rand.RandomColour()
-# generator = gradient.Gradient(gridSize)
-# generator = knuth.Multiplactive(gridSize)
-# generator = jenkins.Mix96(gridSize)
-# generator = wang.Mix32Bit(gridSize)
-# generator = jenkins.Mix32Bit(gridSize)
-# generator = wang.MultiplicationHash(gridSize)
-# generator = wang.Mix64Bit(gridSize)
-# md5hash = builtin.Hashing(gridSize, "mhlem")
-# generator = md5hash
-# generator = perlin.Linear(gridSize, md5hash, 4, 0.485)
-generator = perlin_ref.PerlinRef(gridSize)
+md5hash = builtin.Hashing(gridSize, "mhlem")
+generators = [
+        rand.RandomColour(),
+        gradient.Gradient(gridSize),
+        knuth.Multiplactive(gridSize),
+        jenkins.Mix96(gridSize),
+        wang.Mix32Bit(gridSize),
+        jenkins.Mix32Bit(gridSize),
+        wang.MultiplicationHash(gridSize),
+        wang.Mix64Bit(gridSize),
+        md5hash,
+        perlin.Linear(gridSize, md5hash, 4, 0.485),
+        perlin_ref.PerlinRef(gridSize),
+    ]
 
 bits = 32
-
-bits_used = [0] * bits
-
-for x,y in [(x,y) for x in range(width) for y in range(height)]:
-    hsh = generator.getHash(x, y)
-    for b in range(len(bits_used)):
-        bits_used[b] += 1 if hsh & (1 << b) else 0
-    print "{{0:0{0:d}b}} : {{0:d}}".format(bits).format(hsh)
-        
-print bits_used
-print "spread: {}".format(max(bits_used) - min(bits_used))
+    
+for generator in generators:
+    bits_used = [0] * bits
+    start = time()
+    
+    for x,y in [(x,y) for x in range(width) for y in range(height)]:
+        hsh = generator.getHash(x, y)
+        for b in range(len(bits_used)):
+            bits_used[b] += 1 if hsh & (1 << b) else 0
+        # print "{{0:0{0:d}b}} : {{0:d}}".format(bits).format(hsh)
+    run_time = time() - start
+    
+    print generator.__class__.__name__
+    print bits_used
+    print "spread: {}, runtime: {}".format(max(bits_used) - min(bits_used), run_time)
+    sys.stdout.flush()
