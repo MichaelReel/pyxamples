90d2c472744d94d2717ea8e3311bf8b7e345fe51
Michael Reel <michael.reel@civica.co.uk>
2016-06-03 09:28:17 +0100

    Changed the detail view to use the generic get_object_or_404 function. Cleaned up unused imports.

--- DjangoTutorial/Tutorial/polls/views.py
+++ DjangoTutorial/Tutorial/polls/views.py
@@ -1,11 +1,10 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 
 # Import models
 from .models import Question
 
 # Create your views here.
-from django.http import HttpResponse, Http404
-from django.template import loader
+from django.http import HttpResponse
 
 def index(request):
     latest_question_list = Question.objects.order_by('-pub_date')[:5]
@@ -15,10 +14,7 @@ def index(request):
     return render(request, 'polls/index.html', context)
     
 def detail(request, question_id):
-    try:
-        question = Question.objects.get(pk=question_id)
-    except Question.DoesNotExist:
-        raise Http404("Question  does not exist!")
+    question = get_object_or_404(Question, pk=question_id)
     return render(request, 'polls/detail.html', {'question': question})
 
 def results(request, question_id):
