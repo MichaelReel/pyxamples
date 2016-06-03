413762e9fbfd451a6923488927570db82aa5e3e4
Michael Reel <michael.reel@civica.co.uk>
2016-06-03 14:57:23 +0100

    Added a (failing) test for the was_published_recently function on the Question model.

--- DjangoTutorial/Tutorial/polls/models.py
+++ DjangoTutorial/Tutorial/polls/models.py
@@ -3,6 +3,7 @@ from __future__ import unicode_literals
 from django.db import models
 from django.utils import timezone
 from django.utils.encoding import python_2_unicode_compatible
+import datetime
 
 # Create your models here.
 
