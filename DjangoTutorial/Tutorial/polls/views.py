4d4e942fce04f243f5abe9f84b9dc79ed3cb05dc
Michael Reel <michael.reel@civica.co.uk>
2016-06-03 08:39:30 +0100

    Edited the index view to display the question text. Still hard-coded though.

--- DjangoTutorial/Tutorial/polls/views.py
+++ DjangoTutorial/Tutorial/polls/views.py
@@ -1,10 +1,15 @@
 from django.shortcuts import render
 
+# Import models
+from .models import Question
+
 # Create your views here.
 from django.http import HttpResponse
 
 def index(request):
-    return HttpResponse("Hello, world. You're at the polls index.")
+    latest_question_list = Question.objects.order_by('-pub_date')[:5]
+    output = ', '.join([q.question_text for q in latest_question_list])
+    return HttpResponse(output)
     
 def detail(request, question_id):
     return HttpResponse("You're looking at question %s." % question_id)
