0c7138959802bdfbb0c4fc13ebb8c85ce9fc679c
Michael Reel <michael.reel@civica.co.uk>
2016-06-06 14:26:23 +0100

    Changed the Choice creation on the admin portal to be part of the question creation rather than a separate screen.

--- DjangoTutorial/Tutorial/polls/admin.py
+++ DjangoTutorial/Tutorial/polls/admin.py
@@ -3,11 +3,15 @@ from django.contrib import admin
 # Register your models here.
 from .models import Question, Choice
 
+class ChoiceInline(admin.StackedInline):
+    model = Choice
+    extra = 3
+
 class QuestionAdmin(admin.ModelAdmin):
     fieldsets = [
         (None,              {'fields' : ['question_text']}),
         ('Date Information',{'fields' : ['pub_date']})
     ]
+    inlines = [ChoiceInline]
 
 admin.site.register(Question, QuestionAdmin)
-admin.site.register(Choice)
