38f2b3de7406b2eca798d82554f29b9dd95ed74e
Michael Reel <michael.reel@civica.co.uk>
2016-06-06 14:01:24 +0100

    Separated the publication date fields out into their own field set section on the admin page.

--- DjangoTutorial/Tutorial/polls/admin.py
+++ DjangoTutorial/Tutorial/polls/admin.py
@@ -4,7 +4,10 @@ from django.contrib import admin
 from .models import Question, Choice
 
 class QuestionAdmin(admin.ModelAdmin):
-    fields = ['pub_date', 'question_text']
+    fieldsets = [
+        (None,              {'fields' : ['question_text']}),
+        ('Date Information',{'fields' : ['pub_date']})
+    ]
 
 admin.site.register(Question, QuestionAdmin)
 admin.site.register(Choice)
