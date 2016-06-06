928061bf0100c6c83fa0f906d3c88d899d9c0097
Michael Reel <michael.reel@civica.co.uk>
2016-06-06 14:29:57 +0100

    Modified the choice display to be a table rather than stacked sections.

--- DjangoTutorial/Tutorial/polls/admin.py
+++ DjangoTutorial/Tutorial/polls/admin.py
@@ -3,7 +3,7 @@ from django.contrib import admin
 # Register your models here.
 from .models import Question, Choice
 
-class ChoiceInline(admin.StackedInline):
+class ChoiceInline(admin.TabularInline):
     model = Choice
     extra = 3
 
