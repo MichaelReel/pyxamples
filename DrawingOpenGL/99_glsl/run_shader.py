0f8f6a97666570645bf61768808d144a6efff391
unknown <mreel@LT10245.civica.root.local>
2016-08-19 17:10:41 +0100

    Added a 'very' simple shader example. :grinning:

---
+++ DrawingOpenGL/99_glsl/run_shader.py
@@ -0,0 +1,41 @@
+import pyglet
+from pyglet.gl import *
+
+import sys
+sys.path.append('../tristam_macdonald')
+from shader import Shader
+
+class ShaderWindow(pyglet.window.Window):
+  def __init__(self, shader_path):
+    super(ShaderWindow, self).__init__(caption = 'julia', width = 512, height = 512)
+
+    self.shader = Shader(
+      ' '.join(open('%s.v.glsl' % shader_path)),
+      ' '.join(open('%s.f.glsl' % shader_path))
+    )
+
+  def on_draw(self):
+    glMatrixMode(GL_PROJECTION)
+    glLoadIdentity()
+    glOrtho(-1., 1., 1., -1., 0., 1.)
+
+    glMatrixMode(GL_MODELVIEW)
+    glLoadIdentity()
+
+    self.shader.bind()
+
+    glBegin(GL_QUADS)
+    glVertex2i(-1, -1)
+    glTexCoord2i(-2, -2)
+    glVertex2f(1, -1)
+    glTexCoord2i(2, -2)
+    glVertex2i(1, 1)
+    glTexCoord2i(2, 2)
+    glVertex2i(-1, 1)
+    glTexCoord2i(-2, 2)
+    glEnd()
+
+    self.shader.unbind()
+
+window = ShaderWindow('shader')
+pyglet.app.run()
