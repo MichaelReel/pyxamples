bfed7c7d230292395bcb57bcfbfab006b7ec65b1
Michael Reel <mykilr@gmail.com>
2016-08-20 21:41:01 +0100

    Managed to pass some int data to the frag shader.

--- DrawingOpenGL/99_glsl/shader.f.glsl
+++ DrawingOpenGL/99_glsl/shader.f.glsl
@@ -1,15 +1,16 @@
 #version 110
+#extension GL_EXT_gpu_shader4 : require
 
 uniform vec2 WindowSize;
 uniform vec2 XY;
+uniform int data[256];
 
 void main() {
   // See: https://www.opengl.org/registry/doc/GLSLangSpec.Full.1.20.8.pdf
 
   float fx = gl_FragCoord[0] / WindowSize[0];
   float fy = gl_FragCoord[1] / WindowSize[1];
-  float fz = gl_FragCoord[2];
-  float fiw = gl_FragCoord[3];
+  float fz = float(data[int(gl_FragCoord[0]) % 256]) / 256.0;
 
   if (distance(gl_FragCoord.xy, XY) < 5.0)
   {
