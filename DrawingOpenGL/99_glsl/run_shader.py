983598ef67465a80e964d1222fa0a49c8f9f0487
Michael Reel <mykilr@gmail.com>
2016-08-20 20:16:28 +0100

    Clicking on the shader window saves a screenshot.

--- DrawingOpenGL/99_glsl/run_shader.py
+++ DrawingOpenGL/99_glsl/run_shader.py
@@ -7,12 +7,12 @@ from shader import Shader
 
 class ShaderWindow(pyglet.window.Window):
   def __init__(self, shader_path):
-    width = 512
-    height = 512
+    self.w = 512
+    self.h = 512
 
-    self.windowSize = (float(width), float(height))
-    self.XY = (float(width / 2), float(height / 2))
-    super(ShaderWindow, self).__init__(caption = 'Shader', width=width, height=height)
+    self.windowSize = (float(self.w), float(self.h))
+    self.XY = (float(self.w / 2), float(self.h / 2))
+    super(ShaderWindow, self).__init__(caption = 'Shader', width=self.w, height=self.h)
 
     self.shader = Shader(
       ' '.join(open('%s.v.glsl' % shader_path)),
@@ -21,6 +21,22 @@ class ShaderWindow(pyglet.window.Window):
 
   def on_mouse_motion(self, x, y, dx, dy):
     self.XY = (float(x), float(y))
+    
+  def on_mouse_drag(self, x, y, dx, dy, buttons, modifiers):
+    self.XY = (float(x), float(y))
+
+  def on_mouse_press(self, x, y, button, modifiers):
+    # Assign a byte buffer (int didn't appear to work - for rgb anyway)
+    a = (GLubyte * (4 * self.w * self.h))(0)
+
+    # Call the (rather slow) pixel read
+    glReadPixels(0, 0, self.w, self.h, GL_RGBA, GL_UNSIGNED_BYTE, a)
+
+    # For debugging purposes, create an image using the data
+    image = pyglet.image.ImageData(self.w, self.h, 'RGBA', a)
+    
+    # Save the image to disk
+    image.save("screenshot.png")
 
   def on_draw(self):
     glMatrixMode(GL_PROJECTION)
