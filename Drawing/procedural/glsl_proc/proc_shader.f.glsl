554b2abdea0dcfda22142cb201eca2ca795de354
Michael Reel <mykilr@gmail.com>
2016-08-21 14:39:56 +0100

    Added a glsl version of the proc gen perlin ref.

---
+++ Drawing/procedural/glsl_proc/proc_shader.f.glsl
@@ -0,0 +1,77 @@
+#version 110
+#extension GL_EXT_gpu_shader4 : enable
+
+#define p_size 512
+#define p_range 256.0
+#define zoom 32.0
+
+uniform vec2 WindowSize;
+uniform int p[p_size];
+
+float getHash(float x, float y, float z); 
+float fade(float t);
+float lerp(float t, float a, float b);
+float grad(int hsh, float x, float y, float z);
+
+void main() {
+
+  float zm = 1.0 / zoom;
+
+  // getHash is not normalised to 0.0 <-> 1.0
+  // it's really somewhere between -1.0 and +1.0
+  float fb = (getHash(
+      gl_FragCoord[0] * zm, 
+      gl_FragCoord[1] * zm, 
+      gl_FragCoord[2] * zm
+  ) + 1.0 ) / 2.0;
+
+  if (fb < 0.0) {
+    gl_FragColor = vec4(0.0, 1.0, 0.0, 1.0);
+  } else if (fb > 1.0) {
+    gl_FragColor = vec4(1.0, 0.0, 1.0, 1.0);
+  } else {
+    gl_FragColor = vec4(fb, fb, fb, 1.0);
+  }
+}
+
+float getHash(float x, float y, float z) {
+  int X    = int(floor(x)) & 255;             // FIND UNIT CUBE THAT
+  int Y    = int(floor(y)) & 255;             // CONTAINS POINT.
+  int Z    = int(floor(z)) & 255;
+  x       -= floor(x);                        // FIND RELATIVE X,Y,Z
+  y       -= floor(y);                        // OF POINT IN CUBE.
+  z       -= floor(z);
+  float u  = fade(x);                         // COMPUTE FADE CURVES 
+  float v  = fade(y);                         // FOR EACH OF X,Y,Z.
+  float w  = fade(z); 
+  int A    = p[X  ]+Y;                        // HASH COORDINATES OF
+  int AA   = p[A  ]+Z;                        // THE 8 CUBE CORNERS,
+  int AB   = p[A+1]+Z;
+  int B    = p[X+1]+Y;
+  int BA   = p[B]  +Z;
+  int BB   = p[B+1]+Z;
+
+  return lerp(w, lerp(v, lerp(u, grad(p[AA  ], x    , y    , z     ),   // AND ADD
+                                 grad(p[BA  ], x-1.0, y    , z     )),  // BLENDED
+                         lerp(u, grad(p[AB  ], x    , y-1.0, z     ),   // RESULTS
+                                 grad(p[BB  ], x-1.0, y-1.0, z     ))), // FROM  8
+                 lerp(v, lerp(u, grad(p[AA+1], x    , y    , z-1.0 ),   // CORNERS
+                                 grad(p[BA+1], x-1.0, y    , z-1.0 )),  // OF CUBE
+                         lerp(u, grad(p[AB+1], x    , y-1.0, z-1.0 ),
+                                 grad(p[BB+1], x-1.0, y-1.0, z-1.0 ))));
+}
+
+float fade(float t) {
+  return t * t * t * (t * (t * 6.0 - 15.0) + 10.0);
+}
+
+float lerp(float t, float a, float b) {
+  return a + t * (b - a);
+}
+
+float grad(int hsh, float x, float y, float z) {
+  int   h = hsh & 15;                       // CONVERT LO 4 BITS OF HASH CODE
+  float u = (h < 8) ? x : y;                // INTO 12 GRADIENT DIRECTIONS.
+  float v = (h < 4) ? y : (h == 12 || h == 14) ? x : z;
+  return (((h & 1) == 0) ? u : -u) + (((h & 2) == 0) ? v : -v);
+}
---
+++ Drawing/procedural/glsl_proc/run_procedural_shader.py
@@ -0,0 +1,84 @@
+import pyglet, os, time
+from pyglet.gl import *
+from shader import Shader
+
+class ShaderWindow(pyglet.window.Window):
+  def __init__(self, shader_path):
+    self.w = 512
+    self.h = 512
+
+    self.windowSize = (float(self.w), float(self.h))
+    super(ShaderWindow, self).__init__(caption = 'Shader', width=self.w, height=self.h)
+
+    self.shader = Shader(
+      ' '.join(open('%s.v.glsl' % shader_path)),
+      ' '.join(open('%s.f.glsl' % shader_path))
+    )
+
+    self.p = []
+    permutation = self.getPermutation()
+    for i in range(512):
+        self.p.append(permutation[i % len(permutation)])
+
+  def on_key_release(self, symbol, modifiers):
+    if symbol == pyglet.window.key.F2:
+      self.saveFromShader()
+
+  def saveFromShader(self):
+    a = (GLubyte * (4 * self.w * self.h))(0)
+    glReadPixels(0, 0, self.w, self.h, GL_RGBA, GL_UNSIGNED_BYTE, a)
+    image = pyglet.image.ImageData(self.w, self.h, 'RGBA', a)
+    scriptPath = os.path.dirname(os.path.realpath(__file__))
+    filePath = scriptPath + "/TESTSAVE_" + time.strftime("%Y%m%d_%H%M%S") + ".png"
+    print "save to {}".format(filePath)
+    image.save(filePath)
+
+  def getPermutation(self):
+    return [
+      151,160,137, 91, 90, 15,131, 13,201, 95, 96, 53,194,233,  7,225,
+      140, 36,103, 30, 69,142,  8, 99, 37,240, 21, 10, 23,190,  6,148,
+      247,120,234, 75,  0, 26,197, 62, 94,252,219,203,117, 35, 11, 32,
+       57,177, 33, 88,237,149, 56, 87,174, 20,125,136,171,168, 68,175,
+       74,165, 71,134,139, 48, 27,166, 77,146,158,231, 83,111,229,122,
+       60,211,133,230,220,105, 92, 41, 55, 46,245, 40,244,102,143, 54,
+       65, 25, 63,161,  1,216, 80, 73,209, 76,132,187,208, 89, 18,169,
+      200,196,135,130,116,188,159, 86,164,100,109,198,173,186,  3, 64,
+       52,217,226,250,124,123,  5,202, 38,147,118,126,255, 82, 85,212,
+      207,206, 59,227, 47, 16, 58, 17,182,189, 28, 42,223,183,170,213,
+      119,248,152,  2, 44,154,163, 70,221,153,101,155,167, 43,172,  9,
+      129, 22, 39,253, 19, 98,108,110, 79,113,224,232,178,185,112,104,
+      218,246, 97,228,251, 34,242,193,238,210,144, 12,191,179,162,241,
+       81, 51,145,235,249, 14,239,107, 49,192,214, 31,181,199,106,157,
+      184, 84,204,176,115,121, 50, 45,127,  4,150,254,138,236,205, 93,
+      222,114, 67, 29, 24, 72,243,141,128,195, 78, 66,215, 61,156,180,
+    ]
+
+  def on_draw(self):
+    glMatrixMode(GL_PROJECTION)
+    glLoadIdentity()
+    glOrtho(-1., 1., 1., -1., 0., 1.)
+
+    glMatrixMode(GL_MODELVIEW)
+    glLoadIdentity()
+
+    self.shader.bind()
+    self.shader.uniformf('WindowSize', *self.windowSize)
+    self.shader.uniformi('p', *self.p)
+
+    glBegin(GL_QUADS)
+    glVertex2i(-1, -1)
+    glTexCoord2i(-2, -2)
+    glVertex2f(1, -1)
+    glTexCoord2i(2, -2)
+    glVertex2i(1, 1)
+    glTexCoord2i(2, 2)
+    glVertex2i(-1, 1)
+    glTexCoord2i(-2, 2)
+    glEnd()
+
+    self.shader.unbind()
+
+window = ShaderWindow('proc_shader')
+pyglet.app.run()
+
+  
