bc3623d5f3403a7d8afd2feaf2869f543704f3e0
Michael Reel <mykilr@gmail.com>
2016-09-19 23:05:32 +0100

    Made names a little less like vocab, added some physical features.

--- Characters/character_creator.py
+++ Characters/character_creator.py
@@ -1,30 +1,49 @@
 from random import Random
 from sys import maxint, path
-
-print (path)
-
 path.append("..\\Markov")
-
-print (path)
-
 from markov import MarkovTable
 
 class Character(dict):
 
-    markov = MarkovTable()
-    with open("names_markov.json", "rb") as jsonFile:
-        markov.readJSON(jsonFile)
+    markovForenames = MarkovTable()
+    with open("markov_tables/forenames_3.json", "rb") as jsonFile:
+        markovForenames.readJSON(jsonFile)
+        
+    markovSurnames = MarkovTable()
+    with open("markov_tables/surnames_3.json", "rb") as jsonFile:
+        markovSurnames.readJSON(jsonFile)
 
     def __init__(self, seed):
         rand = Random(seed)
         self.createName(rand.randint(-maxint - 1, maxint))
+        self.createAppearance(rand.randint(-maxint - 1, maxint))
 
     def createName(self, seed):
         rand = Random(seed)
-        self['name'] = Character.markov.makeRandomWord(rand)
+        self['forename'] = Character.markovForenames.makeRandomWord(rand).strip()
+        self['surname'] = Character.markovSurnames.makeRandomWord(rand).strip()
+
+    def createAppearance(self, seed):
+        rand = Random(seed)
+        self['stature'] = ( rand.choice(['short ', '', 'tall ']) +
+                            rand.choice(['thin ', '', 'broad ']) ).strip()
+        self['hair'] = ( rand.choice(['balding ', 'cropped ', 'short ', 'eye-length ', 'neck-length ', 'shoulder-length ', 'waist-length ', '']) +
+                         rand.choice(['tight-curled ', 'curly ', 'wavy ', 'straight ', '']) +
+                         rand.choice(['white ', 'grey ', 'blond ', 'fair ', 'red ', 'light brown ', 'brown ', 'dark brown ', 'black ', '']) ).strip()
+        self['eyes'] = ( rand.choice(['brown ', 'hazel ', 'blue ', 'green ', 'grey ', 'amber ', 'black ', 'pink ']) +
+                         rand.choice(['', 'wide', 'sad', 'clear', 'happy', 'narrow'])).strip()
+
+    def __str__(self):
+
+        return ("---------------------------------------------------\n"
+               "{forename} {surname}\n"
+               "----------------------------------------------------\n"
+               "{stature}\n"
+               "{hair} hair\n"
+               "{eyes} eyes\n"
+               "").format(**self)
 
 
-
-
-char = Character(1)
-print (char)
+chars = [Character(x) for x in range(10)]
+for char in chars:
+    print (char)
