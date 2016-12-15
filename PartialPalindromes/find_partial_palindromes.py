396976fb35823b4af6e98c56aff0308c2b72b3d2
unknown <mreel@LT10245.civica.root.local>
2016-12-15 09:53:25 +0000

    Updated to be more obvious.

--- PartialPalindromes/find_partial_palindromes.py
+++ PartialPalindromes/find_partial_palindromes.py
@@ -1,14 +1,28 @@
+"""
+This will read the dictionary file and output any partial palindromes
+
+A partial palindrome is defined as a word that would
+be a palindrome if the first or last letter was removed.
+"""
 
 INPUT_FILE = "dictionary.txt"
 
+def is_partial_palindrome(word):
+    """
+    Returns true if a word is a partial palindrome
+    """
+    if len(word) < 3:
+        return False
+    return is_palindrome(word[1::]) or is_palindrome(word[:-1:])
+
+def is_palindrome(word):
+    """
+    Returns true if a word is a palindrome
+    """
+    return word == word[::-1]
+
 with open(INPUT_FILE) as inf:
     for line in inf.read().splitlines():
-        if len(line) < 3:
-            continue
-        partial = line[1::]
-        if partial == (partial[::-1]):
-            print line
-        partial = line[:-1:]
-        if partial == (partial[::-1]):
+        if is_partial_palindrome(line):
             print line
 
